9e7a1abcf54c686eefab0b2b98c112b1c20e03b3
Drend <byronthyto@gmail.com>
2024-06-18 13:42:40 -0500

    Add abou parte one

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-	
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      gridTemplateColumns: {
+        "3fr-5fr": "3fr 5fr",
+      },
+    },
+  },
+  plugins: [],
+};
