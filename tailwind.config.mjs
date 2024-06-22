40f22586ad00b3627e8f8b42bbb5a0ba75ffbf0a
Drend <byronthyto@gmail.com>
2024-06-21 22:47:37 -0500

    Add primer componente

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -7,18 +7,23 @@ export default {
         "3fr-5fr": "3fr 5fr",
       },
       flex: {
-        '2': '2 2 0%',
-        '3': '3 3 0%',
+        2: "2 2 0%",
+        3: "3 3 0%",
       },
-     transitionDuration: {
-        '100': '100ms',
+      transitionDuration: {
+        100: "100ms",
       },
-      colors:{
-        'hc1': '#264653',
-        'hc2': '#2a9d8f',
-        'hc3': '#e9c46a',
-        'hc4': '#f4a261',
-        'hc5': '#e76f51'
+      colors: {
+        hc1: "#264653",
+        hc2: "#2a9d8f",
+        hc3: "#e9c46a",
+        hc4: "#f4a261",
+        hc5: "#e76f51",
+      },
+      backgroundImage: {
+        'footer': "url('./src/assets/img/murcielago-footer.png')",
+        'logo': "url('./src/assets/img/murcielago.png')",
+        'fondo': "url('./src/assets/img/fondo.jpeg')",
       }
     },
   },
