606e3c3a324312e9e0ffc08551d9070b0f089e62
Byron <byronthyto@gmail.com>
2024-07-11 17:37:29 -0500

    Primer version de registro de ordenes diarias con cards

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -24,7 +24,10 @@ export default {
         'footer': "url('https://images.pexels.com/photos/2246476/pexels-photo-2246476.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')",
         'logo': "url('./src/assets/img/murcielago.png')",
         'fondo': "url('https://images.pexels.com/photos/33545/sunrise-phu-quoc-island-ocean.jpg')",
-      }
+      },
+      spacing:{
+        '97': '97%',
+      },
     },
   },
   plugins: [],
